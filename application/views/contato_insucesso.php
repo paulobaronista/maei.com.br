0bb4764b03654d4e00386e54d2540234e9fc285d
PauloBaronista <paulo@rpm.com.br>
2019-08-25 09:52:45 -0300

    obrigado / fail

--- application/views/contato_insucesso.php
+++ application/views/contato_insucesso.php
@@ -1,6 +1,334 @@
-<div class="group-form text-center avisos-fail">
-    <p>O e-mail não pode ser enviado!<br />
-        <strong>Tente novamente!</strong><br />
-        Se o problema persistir, entre em contato pelo e-mail <strong>sac@sac.com</strong><br />
-        Ou Ligue: <strong>(xxx) xxxx-xxxx</strong></p>
+<div id="home" class="home">
+    <section class="carousel-default">
+        <div id="carousel-default" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img class="img-responsive" src="<?= base_url(); ?>assets/images/banner-1.jpg" alt="Banner">
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="<?= base_url(); ?>assets/images/banner-2.jpg" alt="Banner">
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="<?= base_url(); ?>assets/images/banner-3.jpg" alt="Banner">
+                </div>
+                <div class="item">
+                    <img class="img-responsive" src="<?= base_url(); ?>assets/images/banner-4.jpg" alt="Banner">
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="logo">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2">
+                    <img class="img-responsive" src="<?= base_url(); ?>assets/images/logo-ma.png" alt="M.A Empreendimentos Imobiliários">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="avisos">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 padding-off-mobile">
+                    <p>O e-mail não pode ser enviado!<br />
+                        <strong>Tente novamente!</strong><br />
+                        Se o problema persistir, entre em contato pelo e-mail <strong>contato@maei.com.br</strong><br />
+                        Ou Ligue: <strong>(11) 3044.0400</strong></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<div id="historia" class="historia">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 text-center">
+                <h2>história</h2>
+                <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/seta.png" alt="seta">
+                <h6>Uma história de realizações. Um futuro de sucesso.</h6>
+                <p>Tradição. Segurança. Qualidade. Estas são as marcas da M.A Empreendimentos Imobiliários
+                    (antiga Munir Abbud Empreendimentos Imobiliários), que ao longo de mais de quatro
+                    décadas vem desenvolvendo dezenas de empreendimentos consagrados, ajudando
+                    escrever a história da construção civil de São Paulo. Porém, nada disso seria possível
+                    sem investimentos constantes em tecnologia e capacitação profissional,
+                    que transformaram a M.A. em sinônimo de pioneirismo e inovação em todos
+                    os segmentos imobiliários. Uma empresa sólida, com uma história repleta de realizações,
+                    um presente de muito trabalho e um futuro altamente promissor. Os próximos 40 anos
+                    da M.A. estão apenas começando. Seja bem-vindo a essa nova jornada.</p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 margin-top-40 text-center">
+                <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/banner-historia-1.jpg" alt="História">
+                <p class="pull-right">tradição.<br>
+                    Segurança.<br>
+                    Qualidade.</p>
+            </div>
+        </div>
+    </div>
+    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/banner-historia-2.jpg" alt="História">
+</div>
+
+<div id="portfolio" class="portfolio">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 tt text-center">
+                <h2>Portfolio</h2>
+                <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/seta.png" alt="seta">
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 padding-off-mobile">
+                <div class="col-xs-12 col-sm-6">
+                    <p class="p-r">empreendimentos <span>comerciais</span></p>
+                </div>
+                <div class="col-xs-12 col-sm-6 padding-off-mobile">
+                    <div class="carousel slide media-carousel" id="comerciais">
+                        <div class="carousel-inner">
+                            <div class="item  active">
+                                <div class="row">
+                                    <div class=" col-xs-12 col-md-12">
+                                        <a href="#" data-toggle="modal" data-target="#Modal-trade-building">
+                                            <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/trade-building.png" alt="Trade Building">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-xs-12 col-md-12">
+                                        <a href="#" data-toggle="modal" data-target="#Modal-Cidade-Jardim">
+                                            <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Cidade-Jardim.png" alt="Cidade Jardim">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-xs-12 col-md-12">
+                                        <a href="#" data-toggle="modal" data-target="#Modal-Tower-Sao-Paulo">
+                                            <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Tower-Sao-Paulo.png" alt="Tower São Paulo">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-xs-12 col-md-12">
+                                        <a href="#" data-toggle="modal" data-target="#Modal-Paulista-The-Office">
+                                            <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Paulista-The-Office.png" alt="Paulista The Office">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a data-slide="prev" href="#comerciais" class="left carousel-control">‹</a>
+                        <a data-slide="next" href="#comerciais" class="right carousel-control">›</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 visible-xs margin-top-20"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 padding-off-mobile">
+                <div class="col-xs-12 col-sm-6 visible-xs">
+                    <p>empreendimentos <span>residenciais</span></p>
+                </div>
+                <div class="col-xs-12 col-sm-6 padding-off-mobile">
+                    <div class="carousel slide media-carousel" id="residenciais">
+                        <div class="carousel-inner">
+                            <div class="item  active">
+                                <div class="row">
+                                    <div class="col-xs-12 col-md-12">
+                                        <a href="#" data-toggle="modal" data-target="#Modal-ed-quasar">
+                                            <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/ed-quasar.png" alt="Ed. Quasar">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-xs-12 col-md-12">
+                                        <a href="#" data-toggle="modal" data-target="#Modal-Palazzo-Ravena">
+                                            <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Palazzo-Ravena.png" alt="Palazzo Ravena">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-xs-12 col-md-12">
+                                        <a href="#" data-toggle="modal" data-target="#Modal-Condominio-Champs-Elysees">
+                                            <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Condominio-Champs-Elysees.png" alt="Condomínio Champs Elysées">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-xs-12 col-md-12">
+                                        <a href="#" data-toggle="modal" data-target="#Modal-Guaruja-Tortugas">
+                                            <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/Guaruja-Tortugas.png" alt="Guarujá Tortugas | Guarujá">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a data-slide="prev" href="#residenciais" class="left carousel-control">‹</a>
+                        <a data-slide="next" href="#residenciais" class="right carousel-control">›</a>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 hidden-xs">
+                    <p>empreendimentos <span>residenciais</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="lancamentos" class="lancamentos">
+    <div class="box-degrade">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 text-center">
+                    <h2>lançamentos</h2>
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/seta.png" alt="seta">
+                    <p>Conheça nossos novos ícones arquitetônicos.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container lancamentos-info">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 padding-off-mobile">
+            <div class="col-xs-12 col-sm-12 col-md-7 text-center">
+                <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/lancamentos.png" alt="Lançamentos">
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-5 text-left">
+                <h3>varanda<br>
+                    cidade jardim</h3>
+
+                <p>273 m2<br>
+                    3 e 2 Suítes<br>
+                    4 Vagas<br>
+                    Depósito Privativo</p>
+                <a href="">Saiba Mais</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="futuros-lancamentos" class="lancamentos-futuros">
+    <div class="box-degrade">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 text-center">
+                    <h2>futuros lancamentos</h2>
+                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/seta.png" alt="seta">
+                    <p>Acompanhe aqui as novidades.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 padding-off-mobile">
+            <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/futuros-lancamentos.jpg" alt="Futuros Lançamentos">
+        </div>
+    </div>
+</div>
+
+<div id="contato" class="contato">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 text-center">
+                <p class="txt">Entre em contato e marque uma reunião com nossos diretores.<br>
+                    Há sempre um bom negócio esperando por você.</p>
+                <h2>Contato</h2>
+                <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/seta.png" alt="seta">
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 margin-top-30">
+                <form method="post" role="form" action="<?php echo base_url("contato") ?>">
+                    <div class="group-form">
+                        <div class="col-xs-12 col-md-12 padding-off-mobile form-group">
+                            <input id="nome" type="text" class="nome form-control" name="nome" required="required" placeholder="Nome:*" />
+                        </div>
+                        <div class="col-xs-12 col-md-5 padding-off-mobile form-group">
+                            <input id="telefone" class="phone form-control" type="tel" name="phone" placeholder="Telefone:" />
+                        </div>
+                        <div class="col-xs-12 col-md-7 padding-off-mobile form-group">
+                            <input id="email" type="email" class="email form-control" name="email" required="required" placeholder="E-mail:*" />
+                        </div>
+                        <div class="col-xs-12 col-md-12 padding-off-mobile form-group">
+                            <input id="interesse" type="text" class="interesse form-control" name="interesse" placeholder="Assunto:" />
+                        </div>
+                        <div class="col-xs-12 col-md-8 padding-off-mobile form-group">
+                            <textarea id="form-mensagem" class="msg form-control" rows="3" name="mss" placeholder="Mensagem:"></textarea>
+                        </div>
+                        <div class="col-xs-12 col-md-4 padding-off-mobile form-group">
+                            <button type="submit" class="btn_enviar enviar btn center-block" title="enviar" name="enviar_email" value="enviar">Enviar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2">
+                    <div class="col-xs-12 col-md-4">
+                        <h6>endereço</h6>
+                        <span>
+                            <img src="<?= base_url(); ?>assets/images/icone-local.png" alt="Endereço">
+                            <p>Rua Funchal, 375 • 13º andar<br>
+                                V. Olímpia • São Paulo/SP<br>
+                                04551-060</p>
+                        </span>
+                    </div>
+                    <div class="col-xs-12 col-md-4">
+                        <h6>Pelo Telefone</h6>
+                        <span>
+                            <img src="<?= base_url(); ?>assets/images/icone-telefone.png" alt="Telefone">
+                            <p><strong>11 3044.0400</strong></p>
+                        </span>
+                    </div>
+                    <div class="col-xs-12 col-md-4">
+                        <h6>Pelo WHATSapp</h6>
+                        <span>
+                            <img src="<?= base_url(); ?>assets/images/icone-whatsapp.png" alt="WhatsApp">
+                            <p><strong>11 9000.0000</strong></p>
+                        </span>
+                    </div>
+                    <div class="col-xs-12 redes">
+                        <ul>
+                            <li>
+                                <a href="">
+                                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/facebook.png" alt="Facebook">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/instagram.png" alt="Instagram">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/twitter.png" alt="Twitter">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/youtube.png" alt="Youtube">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+<?php
+if (isset($email_enviado))
+    echo $email_enviado;
+?>
