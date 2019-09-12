b4e9266d1029088e3fdee31b30bcd39e9474d4d9
PauloBaronista <paulo@rpm.com.br>
2019-09-11 21:29:55 -0300

    Ajuste cliente

--- application/views/rodape.php
+++ application/views/rodape.php
@@ -1,6 +1,11 @@
 <div class="modal fade" id="Modal-trade-building" tabindex="-1" role="dialog" aria-labelledby="Modal-trade-building">
     <div class="modal-dialog modal-lg" role="document">
         <a class="close" data-dismiss="modal" aria-label="Close" href="">X</a>
+        <div class="col-sm-12 text-center">
+            <p>SÃO PAULO TRADE BUILDING<br>
+                15 ANDADRES – 420 M²<br>
+                VILA OLIMPIA</p>
+        </div>
         <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/full-trade-building.png" alt="Trade Building">
     </div>
 </div>
@@ -32,36 +37,66 @@
 <div class="modal fade" id="Modal-Tower-Sao-Paulo" tabindex="-1" role="dialog" aria-labelledby="Modal-Tower-Sao-Paulo">
     <div class="modal-dialog modal-lg" role="document">
         <a class="close" data-dismiss="modal" aria-label="Close" href="">X</a>
+        <div class="col-sm-12 text-center">
+            <p>E-TOWER SÃO PAULO<br>
+                36 ANDARES - 635 A 1820 M²<br>
+                VILA OLIMPIA</p>
+        </div>
         <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/full-Tower-Sao-Paulo.png" alt="Tower São Paulo">
     </div>
 </div>
 <div class="modal fade" id="Modal-Paulista-The-Office" tabindex="-1" role="dialog" aria-labelledby="Modal-Paulista-The-Office">
     <div class="modal-dialog modal-lg" role="document">
         <a class="close" data-dismiss="modal" aria-label="Close" href="">X</a>
+        <div class="col-sm-12 text-center">
+            <p>PAULISTA PLAZA THE OFFICE<br>
+                192 CONJUNTOS COMERCIAIS<br>
+                PARAISO</p>
+        </div>
         <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/full-Paulista-The-Office.jpg" alt="Paulista The Office">
     </div>
 </div>
 <div class="modal fade" id="Modal-ed-quasar" tabindex="-1" role="dialog" aria-labelledby="Modal-ed-quasar">
     <div class="modal-dialog modal-lg" role="document">
         <a class="close" data-dismiss="modal" aria-label="Close" href="">X</a>
+        <div class="col-sm-12 text-center">
+            <p>QUASAR<br>
+                275 M²<br>
+                MOEMA</p>
+        </div>
         <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/full-ed-quasar.png" alt="Ed. Quasar">
     </div>
 </div>
 <div class="modal fade" id="Modal-Palazzo-Ravena" tabindex="-1" role="dialog" aria-labelledby="Modal-Palazzo-Ravena">
     <div class="modal-dialog modal-lg" role="document">
         <a class="close" data-dismiss="modal" aria-label="Close" href="">X</a>
+        <div class="col-sm-12 text-center">
+            <p>PALAZZO RAVENA<br>
+                352 M²<br>
+                JARDINS</p>
+        </div>
         <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/full-Palazzo-Ravena.png" alt="Palazzo Ravena">
     </div>
 </div>
 <div class="modal fade" id="Modal-Condominio-Champs-Elysees" tabindex="-1" role="dialog" aria-labelledby="Modal-Condominio-Champs-Elysees">
     <div class="modal-dialog modal-lg" role="document">
         <a class="close" data-dismiss="modal" aria-label="Close" href="">X</a>
+        <div class="col-sm-12 text-center">
+            <p>COND. CHAMPS ELYSÉES<br>
+                9 TORRES – 288 APTOS<br>
+                GRANJA JULIETA</p>
+        </div>
         <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/full-Condominio-Champs-Elysees.jpg" alt="Condomínio Champs Elysées">
     </div>
 </div>
 <div class="modal fade" id="Modal-Guaruja-Tortugas" tabindex="-1" role="dialog" aria-labelledby="Modal-Guaruja-Tortugas">
     <div class="modal-dialog modal-lg" role="document">
         <a class="close" data-dismiss="modal" aria-label="Close" href="">X</a>
+        <div class="col-sm-12 text-center">
+            <p>TORTUGA´S<br>
+                5 TORRES – 278 APTOS<br>
+                GUARUJÁ</p>
+        </div>
         <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/full-Guaruja-Tortugas.jpg" alt="Guarujá Tortugas | Guarujá">
     </div>
 </div>
